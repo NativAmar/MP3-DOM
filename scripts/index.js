6cba0763befe2a8da2886c4c95c6beec238daf64
NativAmar <89576789+NativAmar@users.noreply.github.com>
2021-09-13 04:04:14 +0300

    playSong

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,14 @@
  * @param {String} songId - the ID of the song to play
  */
  function playSong(songId) {
-    // Your code here
+    const selectedSong = document.getElementsByName(songId);
+    const classes = []
+    classes.push(["selected"])
+    const songs = document.getElementsByClassName("song");
+    for (let song of songs) {
+        song.classList.remove(classes)
+    }
+    selectedSong.classList.add(classes);
 }
 
 
